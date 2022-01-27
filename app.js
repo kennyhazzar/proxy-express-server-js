62a7bad56e018b2cc33d17be81c631a9b15026c8
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2022-01-28 01:27:10 +0700

    init project

---
+++ app.js
@@ -0,0 +1,11 @@
+﻿import express from 'express'
+import router from './routes/index.js'
+import 'dotenv/config'
+
+const app = express()
+const PORT = 8080
+
+app.use(express.json())
+app.use(router)
+
+app.listen(PORT, () => console.log(`server has been started in ${PORT}`))
---
+++ routes/index.js
@@ -0,0 +1,76 @@
+﻿import axios from 'axios'
+import { response, Router } from 'express'
+const router = new Router()
+import 'dotenv/config'
+
+router.get('/index.php', async (req, res) => {
+
+    // get_users    
+    if (req.query.hasOwnProperty('/api/v2/get_users')) {
+        try {
+            const token = req.headers.authorization
+
+            if (!token) {
+                return res.status(401).send({ error: "token does not exist" })
+            }
+
+            const response = await axios.get(`https://${process.env.server}/index.php?/api/v2/get_users`, {
+                headers: {
+                    "Authorization": token,
+                    "Content-Type": "application/json"
+                }
+            })
+
+            return res.send(response.data)
+        } catch (error) {
+            return res.status(401).send({ error: error.response.data.error })
+        }
+    }
+    // get_statuses
+    if (req.query.hasOwnProperty('/api/v2/get_statuses')) {
+        try {
+            const token = req.headers.authorization
+
+            if (!token) {
+                return res.status(401).send({ error: "token does not exist" })
+            }
+
+            const response = await axios.get(`https://${process.env.server}/index.php?/api/v2/get_statuses`, {
+                headers: {
+                    "Authorization": token,
+                    "Content-Type": "application/json"
+                }
+            })
+
+            return res.send(response.data)
+        } catch (error) {
+            return res.status(401).send({ error: error.response.data.error })
+        }
+    }
+    //get user by email. params: email={email}
+    if (req.query.hasOwnProperty('/api/v2/get_user_by_email')) {
+        try {
+
+            const token = req.headers.authorization
+
+            if (!token) {
+                return res.status(401).send({ error: "token does not exist" })
+            }
+
+            const response = await axios.get(`https://${process.env.server}/index.php?/api/v2/get_user_by_email&email=${req.query.email}`, {
+                headers: {
+                    "Authorization": token,
+                    "Content-Type": "application/json"
+                }
+            })
+
+            return res.send(response.data)
+        } catch (error) {
+            return res.status(401).send({ error: error.response.data.error })
+        }
+    }
+
+    return res.status(500).send({ error: "server error" })
+})
+
+export default router
