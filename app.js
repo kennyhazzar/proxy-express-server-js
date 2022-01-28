4b5894d76c864288a5e9aab4c25c7a54fff4b34e
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2022-01-28 15:24:50 +0700

    fix/add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 ﻿import express from 'express'
 import router from './routes/index.js'
 import 'dotenv/config'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
 app.use(router)
+app.use(cors())
 
 app.listen(process.env.PORT, () => console.log(`server has been started in ${process.env.PORT}`))
