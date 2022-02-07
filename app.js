d2780238d7a2cfd801b2a5034d5853e5376acc4c
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2022-02-07 23:32:15 +0700

    fix/add cors middleware, remove cors (node-module)

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 ﻿import express from 'express'
 import router from './routes/index.js'
 import 'dotenv/config'
-import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*'])
+    res.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+    res.append('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
 app.use(router)
-app.use(cors())
 
 app.listen(process.env.PORT, () => console.log(`server has been started in ${process.env.PORT}`))
